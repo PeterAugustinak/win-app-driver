daae90957d1d5f881f6aa2b0454c092b5f8b7c49
PeterAugustinak <48356585+PeterAugustinak@users.noreply.github.com>
2021-02-01 10:57:10 +0100

    chapter 7 - xpath

--- calculator_test.py
+++ calculator_test.py
@@ -51,6 +51,7 @@ class CalculatorTest(unittest.TestCase):
         text = text.strip("Display is ").rstrip(" ").lstrip(" ")
         return text
 
+    # BY CLASS_NAME
     def choose_calculator(self, locator):
         print(f"Selected calc: {locator}")
 
@@ -77,4 +78,37 @@ class CalculatorTest(unittest.TestCase):
         print(f"Selecting another calc ...")
         self.choose_calculator("Scientific")
 
+    # BY XPATH
+    def choose_calculator_xpath(self, locator):
+        print(f"Selected calc: {locator}")
 
+        # open the hamburger combo
+        self.calcsession.find_element_by_accessibility_id("TogglePaneButton").click()
+        # read all items in combo into the list
+        lst_of_elements_in_menu = self.calcsession.find_elements_by_xpath("//ListItem")
+
+        print(f"Calculator combo elements: {len(lst_of_elements_in_menu)} -> "
+              f"{', '.join([item.get_attribute('Name') for item in lst_of_elements_in_menu])}")
+
+        # find item by locator inputed and click on it
+        for item in lst_of_elements_in_menu:
+            if item.get_attribute("Name") == locator:
+                item.click()
+                # if locator was found, stop looking further
+                break
+
+    def test_choose_another_calc_xpath(self):
+        print(f"Selecting another calc ...")
+        self.choose_calculator_xpath("Scientific Calculator")
+
+    # BY XPATH SIMPLE
+    def choose_calculator_xpath_simple(self, locator):
+        print(f"Selected calc: {locator}")
+
+        # open the hamburger combo
+        self.calcsession.find_element_by_accessibility_id("TogglePaneButton").click()
+        self.calcsession.find_element_by_xpath(f"//ListItem[contains(@Name, \"{locator}\")]").click()
+
+    def test_choose_another_calc_xpath_simple(self):
+        print(f"Selecting another calc ...")
+        self.choose_calculator_xpath_simple("Scientific Calculator")
